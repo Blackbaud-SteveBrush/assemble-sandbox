45cd7c6fa0fa53c6e451919ef0010e0bb90cf1f7
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2015-11-22 10:48:15 -0500

    Separating stache from project

--- gruntfile.js
+++ gruntfile.js
@@ -1,49 +1,8 @@
+'use strict';
+
 module.exports = function (grunt) {
-    grunt.initConfig({
-        assemble: {
-            site: {
-                options: {
-                    layout: './layouts/default.hbs'
-                },
-                src: ['pages/*.hbs'],
-                dest: './site/'
-            }
-        },
-        sass: {
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: 'styles',
-                    src: ['*.scss'],
-                    dest: './site/pages/css',
-                    ext: '.css'
-                }]
-            }
-        },
-        connect: {
-            server: {
-                options: {
-                    port: 9001,
-                    base: './site/pages/',
-                    livereload: true
-                }
-            },
-        },
-        watch: {
-            pages: {
-                files: ['pages/*.hbs', 'layouts/*.hbs'],
-                tasks: ['assemble:site']
-            },
-            styles: {
-                files: ['styles/*.scss'],
-                tasks: ['sass:dist']
-            }
-        }
+    grunt.task.loadNpmTasks('stache-barebones');
+    grunt.task.registerTask('default', function() {
+        grunt.task.run('serve');
     });
-    grunt.loadNpmTasks('assemble');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('build', ['assemble:site', 'sass:dist']);
-    grunt.registerTask('serve', ['assemble:site', 'sass:dist', 'connect:server', 'watch']);
 };
